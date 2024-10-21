d11d3f244fc95cc57294ea16f827e26b48ce2c5b
BlackCat <iamakid005@gmail.com>
2024-10-22 02:15:42 +0700

    Add files via upload

---
+++ Ether Drops/coins.js
@@ -0,0 +1,32 @@
+[
+  {
+    symbol: 'BTC',
+  },
+  {
+    symbol: 'ETH',
+  },
+  {
+    symbol: 'SOL',
+  },
+  {
+    symbol: 'DOGE',
+  },
+  {
+    symbol: 'TRX',
+  },
+  {
+    symbol: 'TON',
+  },
+  {
+    symbol: 'PEPE',
+  },
+  {
+    symbol: 'SHIB',
+  },
+  {
+    symbol: 'NOT',
+  },
+  {
+    symbol: 'HMSTR',
+  },
+];
